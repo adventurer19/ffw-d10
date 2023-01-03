f35b2fd9b76428dbf587c7de81cd0a5dd95fef91
Nikolay Penchev <nikolay.penchev@ffwagency.com>
2023-01-03 17:00:32 +0200

    settings php

--- drupal-dir/web/sites/default/settings.php
+++ drupal-dir/web/sites/default/settings.php
@@ -264,7 +264,7 @@ $databases = [];
  *   $settings['hash_salt'] = file_get_contents('/home/example/salt.txt');
  * @endcode
  */
-$settings['hash_salt'] = '11RvG5YPyjfyb1oNH19smevtb5ZcKf2xmqRoyXMIuad27_r3ktBCDyMpuhIWTrUQqg2jVZektA';
+$settings['hash_salt'] = '';
 
 /**
  * Deployment identifier.
@@ -813,15 +813,3 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
-$databases['default']['default'] = array (
-  'database' => 'drupal',
-  'username' => 'drupal',
-  'password' => 'drupal',
-  'prefix' => '',
-  'host' => 'localhost',
-  'port' => '3306',
-  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
-  'driver' => 'mysql',
-  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
-);
-$settings['config_sync_directory'] = '../config';
